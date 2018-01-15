d5ac85fc466510d9eb8026103c62fb96370eeefb
Rodrigo Landaeta <rlandaeta@cochlear.com>
2018-01-15 18:02:32 +1100

    Initialised the project to test animatable

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { AppRegistry, View, Text } from "react-native";
+
+class App extends React.PureComponent {
+    public render() {
+        return (
+            <View>
+            </View>
+        );
+    }
+}
+
+AppRegistry.registerComponent('animatableApp', () => App);
