d30f85907a834e78b89f851a3bfce2c38e427112
Rodrigo Landaeta <rlandaeta@cochlear.com>
2018-01-15 22:30:16 +1100

    Initial commit, trying to get the typescript boiler plate out of the way

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,19 @@
 import * as React from "react";
-import { AppRegistry, View, Text } from "react-native";
+import { AppRegistry, View, Text, ScrollView, TouchableWithoutFeedback } from "react-native";
+import * as Animatable from "react-native-animatable";
 
 class App extends React.PureComponent {
     public render() {
         return (
-            <View>
-            </View>
+            <ScrollView style={{ flex: 1 }}>
+                <View style={{ padding: 30 }}>
+                    <TouchableWithoutFeedback>
+                        <Animatable.View style={{ backgroundColor: "darkred" }} animation="bounce" iterationCount="infinite">
+                            <Text style={{ textAlign: "center" }}>Button</Text>
+                        </Animatable.View>
+                    </TouchableWithoutFeedback>
+                </View>
+            </ScrollView>
         );
     }
 }
